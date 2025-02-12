2a16bbdaa55bab4250693066610eae4604021217
Bruno Aguiar <aguiarsilva@gmail.com>
2025-02-12 14:05:22 +0100

    fixed errors in ex00 and ex01

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,32 +6,24 @@
 /*   By: baguiar- <baguiar-@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/14 11:45:26 by baguiar-          #+#    #+#             */
-/*   Updated: 2025/01/14 12:54:38 by baguiar-         ###   ########.fr       */
+/*   Updated: 2025/02/12 14:04:28 by baguiar-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <cctype>
 
 int main(int argc, char **argv)
 {
-    int i = 1; int j = 0; char c_upper='\0';
-    
     if (argc < 2)
         std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
     else
     {
-        while (argv[i])
+        for (int i = 1; i < argc; i++)
         {
-            j = 0;
-            while (argv[i][j])
-            {
-                c_upper = std::toupper(argv[i][j]);
-                std::cout << c_upper;
-                j++;
-            }
-            if (argv[i + 1])
-                std::cout << " ";
-            i++;
+            std::string str = argv[i];
+            for (size_t j = 0; j < str.length(); j++)
+                std::cout << (char)std::toupper(str[j]);
         }
         std::cout << std::endl;
         return (0);
