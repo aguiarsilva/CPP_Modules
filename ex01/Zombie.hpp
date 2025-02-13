9752c0e2cf233dc47672d82af50aabc1dc277bee
Bruno Aguiar <aguiarsilva@gmail.com>
2025-02-13 13:55:20 +0100

    removed files unused

---
+++ ex01/Zombie.hpp
@@ -0,0 +1,18 @@
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+
+#include <string>
+
+class Zombie
+{
+    public:
+        Zombie(std::string name);
+        ~Zombie();
+        void announce(void);
+    private:
+        std::string name;
+};
+
+Zombie* zombieHorde(int N, std::string name);
+
+#endif
