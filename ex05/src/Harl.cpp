b2ef4a23ebd34c1dbdff49d4cdabbf1c8c5c1c99
Bruno Aguiar <aguiarsilva@gmail.com>
2025-02-17 23:31:33 +0100

    fixed outputs

--- ex05/src/Harl.cpp
+++ ex05/src/Harl.cpp
@@ -33,24 +33,24 @@ void Harl::complain(std::string level)
 //private methods:
 void Harl::debug(void)
 {
-    std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!" << std::endl;
+    std::cout << " [ DEBUG ] I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!" << std::endl;
     return ;
 }
 
 void Harl::info(void)
 {
-    std::cout << "I cannot believe adding extra bacont consts more money. You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!" << std::endl;
+    std::cout << " [ INFO ] I cannot believe adding extra bacont consts more money. You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!" << std::endl;
     return ;
 }
 
 void Harl::warning(void)
 {
-    std::cout << "I think I deserve to have some extra bacon for free. I've been coming for years whereas you started working here since last month." << std::endl;
+    std::cout << " [ WARNING ] I think I deserve to have some extra bacon for free. I've been coming for years whereas you started working here since last month." << std::endl;
     return ;
 }
 
 void Harl::error(void)
 {
-    std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+    std::cout << " [ ERROR ] This is unacceptable! I want to speak to the manager now." << std::endl;
     return ;
 }
