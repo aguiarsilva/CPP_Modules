f2bf4f8c7951506d26cf3399427e0dd0c26c83c2
Bruno Aguiar <aguiarsilva@gmail.com>
2025-07-06 23:53:03 +0200

    WIP: main.cpp with the tests

---
+++ ex02/src/main.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: baguiar- <baguiar-@student.42wolfsburg.de  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/06 23:40:37 by baguiar-          #+#    #+#             */
+/*   Updated: 2025/07/06 23:52:24 by baguiar-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/Array.hpp"
+#include <iostream>
+#include <exception>
+#include <string>
+
+int main()
+{
+    try
+    {
+        
+    }
+    catch (const std::exception& e)
+    {
+        std::cout << "Exception: " << e.what() << std::endl;
+    }
+    return 0;
+}
+
